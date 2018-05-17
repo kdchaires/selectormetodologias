4395562de7f689388cfb233dc10d981f36096a88
kdchaires <karina.chaires@cimat.mx>
2018-05-17 16:37:55 -0500

    [#HU-03] Se agrega material desing

--- app/src/Questions.elm
+++ app/src/Questions.elm
@@ -13,7 +13,7 @@ budgetAvailableQuestion : Question
 budgetAvailableQuestion =
     { id = 1
     , question = "¿El presupuesto para el proyecto es limitado?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "No"
     }
 
@@ -22,7 +22,7 @@ projectDimensionQuestion : Question
 projectDimensionQuestion =
     { id = 2
     , question = "¿Se puede considerar grande la dimensión del proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "No"
     }
 
@@ -31,7 +31,7 @@ deliveryTimeQuestion : Question
 deliveryTimeQuestion =
     { id = 3
     , question = "¿Es necesario entregar el proyecto en un corto período de tiempo, en relación con el tamaño del proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "No"
     }
 
@@ -40,7 +40,7 @@ timeBoxingQuestion : Question
 timeBoxingQuestion =
     { id = 4
     , question = "¿Están dispuestos a eliminar funciones secundarias de SW para cumplir con el cronograma del proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
@@ -49,7 +49,7 @@ requiredDocumentationQuestion : Question
 requiredDocumentationQuestion =
     { id = 5
     , question = "¿Es necesario que el proyecto genere documentación sólida?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
@@ -58,7 +58,7 @@ developersQuestion : Question
 developersQuestion =
     { id = 6
     , question = "¿Se requiere un equipo de desarrollo sólido y completo en el ¿proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
@@ -67,7 +67,7 @@ clientsQuestion : Question
 clientsQuestion =
     { id = 6
     , question = "¿Es necesario en el proyecto acompañar al cliente en el proceso o varias etapas de eso?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
@@ -76,7 +76,7 @@ changesQuestion : Question
 changesQuestion =
     { id = 7
     , question = "¿El proyecto a desarrollar puede presentar cambios significativos en cualquier etapa o etapa del proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
@@ -85,7 +85,7 @@ iterativeQuestion : Question
 iterativeQuestion =
     { id = 8
     , question = "¿Se requiere realizar iteraciones en el desarrollo del proyecto?"
-    , choices = [ "Si, por mucho", "Si", "No tanto", "No, para nada" ]
+    , choices = [ "Si", "No" ]
     , correctAnswer = "Si"
     }
 
