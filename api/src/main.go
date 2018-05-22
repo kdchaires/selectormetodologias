e988bd1339886e88a0351dc252fa46f715d70a25
Julio César <zzantares@gmail.com>
2018-05-22 11:33:56 -0500

    [HU3] Lee recurso "questions/" desde la base de datos
    Ahora las preguntas que se devuelven desde "questions/" son leídas de
    la base de datos, es necesario ejecutar "dep ensure" para instalar el
    conector con la DB MongoDB.

--- api/src/main.go
+++ api/src/main.go
@@ -6,22 +6,62 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Still alive!")
 }
 
-func questionsList(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(QuestionsList())
+func DatabaseCheckHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := mgo.Dial("localhost:27017")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var q Question
+
+	// modify structure to take the mongo ID and remove the published at
+	c := session.DB("selector_metodologias").C("questions")
+	oerr := c.Find(bson.M{}).One(&q)
+
+	if oerr != nil {
+		log.Fatal(oerr)
+	}
+
+	json.NewEncoder(w).Encode(q)
+}
+
+func QuestionsListHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := mgo.Dial("localhost:27017")
+
+	if err != nil {
+		json.NewEncoder(w).Encode("Can't connect to the database, check connection setting")
+		return
+	}
+
+	var questions []Question
+	// TODO Read DB settings from environment
+	c := session.DB("selector_metodologias").C("questions")
+	oerr := c.Find(bson.M{}).All(&questions)
+
+	if oerr != nil {
+		log.Fatal("Can't read from database, check permissions and resource names")
+		return
+	}
+
+	json.NewEncoder(w).Encode(questions)
 }
 
 func main() {
 	var router = mux.NewRouter()
-	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
-	router.HandleFunc("/questions", questionsList).Methods("GET")
+	router.HandleFunc("/healthcheck", HealthCheckHandler).Methods("GET")
+	router.HandleFunc("/questions", QuestionsListHandler).Methods("GET")
+	router.HandleFunc("/dbcheck", DatabaseCheckHandler).Methods("GET")
 
 	headersOk := handlers.AllowedHeaders([]string{"Authorization"})
 	originsOk := handlers.AllowedOrigins([]string{"*"})
@@ -29,5 +69,6 @@ func main() {
 	middlewareCORS := handlers.CORS(originsOk, headersOk, methodsOk)
 
 	fmt.Println("Running server!")
-	log.Fatal(http.ListenAndServe(":3000", middlewareCORS(router)))
+	// TODO Read bind address and port from environment
+	log.Fatal(http.ListenAndServe(":8000", middlewareCORS(router)))
 }
