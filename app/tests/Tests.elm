13e6a6f8098f945b2cd297fd0106c5a296b2dc9c
Julio César <zzantares@gmail.com>
2018-06-01 21:05:12 -0500

    [HU3] Agrega pruebas a los decoders de Question
    Hacen falta más pruebas claro, pero los decoders siempre son claves.

--- app/tests/Tests.elm
+++ app/tests/Tests.elm
@@ -1,20 +1,38 @@
 module Tests exposing (..)
 
+-- TODO Later organize tests in different modules
+
 import Test exposing (..)
 import Expect
 import String
+import Main
+import SampleResponses as Sample
+import Json.Decode
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
+    describe "Questions decoders"
+        [ test "Correctly decodes a Question" <|
             \() ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
+                let
+                    question =
+                        Main.Questions "1" "Es el presupuesto para el proyecto limitado?" "Dimensión del proyecto"
+
+                    decodedQuestion =
+                        Sample.questionResponse
+                            |> Json.Decode.decodeString Main.questionDecoder
+                in
+                    Expect.equal decodedQuestion (Ok question)
+        , test "Correctly decodes a List of Question" <|
             \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+                let
+                    questions =
+                        [ Main.Questions "1" "Es el presupuesto para el proyecto limitado?" "Dimensión del proyecto" ]
+
+                    decodedQuestions =
+                        Sample.questionsResponse
+                            |> Json.Decode.decodeString Main.questionListDecoder
+                in
+                    Expect.equal decodedQuestions (Ok questions)
         ]
