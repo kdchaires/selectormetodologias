61dc745723b160cbe8b92013efd4a9003bb0815e
Julio César <zzantares@gmail.com>
2018-06-01 19:59:35 -0500

    [HU2] Feedback utiliza "email" como ID para facilitar implementación
    Esto ayuda dado que al guardar a MongoDB no se puede obtener el ID del
    nuevo documento, la manera en la que debe hacerse es que uno debe
    generar el ID, pero de ser así entonces no tiene sentido que se genere
    uno, mejor utilizar el email y esto además ayuda a que no existan
    emails duplicados en la base de datos.

--- api/models/feedback.go
+++ api/models/feedback.go
@@ -10,17 +10,14 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/globalsign/mgo/bson"
 )
 
 type Feedback struct {
-	ID          bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	CreatedAt   time.Time     `json:"created_at" bson:"created_at"`
-	Institution string        `json:"institution"`
-	Email       string        `json:"email"`
-	Finished    bool          `json:"finished"`
-	Description string        `json:"description"`
+	Email       string    `json:"email" bson:"_id"`
+	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
+	Institution string    `json:"institution"`
+	Finished    bool      `json:"finished"`
+	Description string    `json:"description"`
 }
 
 func (db *DB) SaveFeedback(feedback *Feedback) error {
