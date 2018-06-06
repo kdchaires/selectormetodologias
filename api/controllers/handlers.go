c50c0ee9ed12736231d74185c0228b05bcee3712
Julio César <zzantares@gmail.com>
2018-06-06 14:36:40 -0500

    Implementa una respuesta falsa (de Apiary) al hacer POST en /suggest

--- api/controllers/handlers.go
+++ api/controllers/handlers.go
@@ -25,5 +25,18 @@ func (app *App) QuestionsListHandler(w http.ResponseWriter, r *http.Request) {
 // https://selectormetodologias1.docs.apiary.io/#reference/sugerencia/coleccion-de-recomendacion/generar-una-recomendacion
 func (app *App) SuggestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("Call OK!")
+	var fakeResponse = `
+{
+    "name": "Scrum",
+    "score" 23,
+    "links": [
+        {
+            "href": "methodologies/223",
+            "rel": "methodologies",
+            "type": "GET"
+        }
+    ]
+}
+`
+	json.NewEncoder(w).Encode(fakeResponse)
 }
